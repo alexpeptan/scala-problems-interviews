348f2028c9f37f8121f03ea4412a525513ec42a5
Alex Peptan <alexandru.peptan.85@gmail.com>
2022-12-03 21:13:35 +0200

    Lists - Generate All Valid Parenthesis

--- src/main/scala/com/rockthejvm/strings/PatenthesisProblem.scala
+++ src/main/scala/com/rockthejvm/strings/PatenthesisProblem.scala
@@ -30,10 +30,52 @@ object PatenthesisProblem extends App {
     checkTailrec(string, 0)
   }
 
-  println(hasValidParentheses(""))
-  println(hasValidParentheses("()()"))
-  println(hasValidParentheses("(())"))
-  println(hasValidParentheses(")("))
-  println(hasValidParentheses(")"))
-  println(hasValidParentheses("(("))
+  /*
+    n = 1 => List("()")
+    n = 2 => List("(())", "()()")
+    n = 3 => List("()()()", "()(())", "(())()", "((()))", "(()())")
+  */
+  // TODO Generate All Parentheses
+  //  Initial time to solve:: 21:30
+  //  Complexity: O(3^n)
+  //  Final time:
+//  def generateAllValidParentheses(n: Int): List[String] = {
+//    @tailrec
+//    def generateTailrec(k: Int, acc: List[String]): List[String] = {
+//      if (k <= 0) acc
+//      else {
+//        val variations: List[String] = acc.flatMap((s: String) => List(s"()$s", s"($s)", s"$s()")).toSet.toList // legit? :D
+//        generateTailrec(k-1, variations)
+//      }
+//    }
+//
+//    generateTailrec(n, List(""))
+//  }
+
+  def generateAllValidParentheses(n: Int): List[String] = {
+    @tailrec
+    def generateTailrec(k: Int, acc: Set[String]): Set[String] = {
+      if (k <= 0) acc
+      else {
+        val variations: Set[String] = acc.flatMap((s: String) => Set(s"()$s", s"($s)", s"$s()"))
+        generateTailrec(k-1, variations)
+      }
+    }
+
+    generateTailrec(n, Set("")).toList
+  }
+
+//  println(hasValidParentheses(""))
+//  println(hasValidParentheses("()()"))
+//  println(hasValidParentheses("(())"))
+//  println(hasValidParentheses(")("))
+//  println(hasValidParentheses(")"))
+//  println(hasValidParentheses("(("))
+
+
+  println(generateAllValidParentheses(-1))
+  println(generateAllValidParentheses(0))
+  println(generateAllValidParentheses(1))
+  println(generateAllValidParentheses(2))
+  println(generateAllValidParentheses(3))
 }
